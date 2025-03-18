fcc6d5fe06bcb6ec70b79d000982659cbd4f2e20
Daniela Baron <baron.daniela@gmail.com>
2025-03-18 09:45:51 -0400

    simplify remove hidden field target for stimulus

--- app/javascript/controllers/file_upload_controller.js
+++ app/javascript/controllers/file_upload_controller.js
@@ -1,32 +1,25 @@
 import { Controller } from "@hotwired/stimulus";
 
-// TODO: Remove hidden field, just need defaultTextValue
 // Connects to data-controller="file-upload"
 export default class extends Controller {
-  static targets = ["input", "hiddenField", "label"];
+  static targets = ["input", "label"];
   static values = {
     defaultText: { type: String, default: 'Choose file...' }
   }
 
   connect() {
-    console.log("=== connect ===");
     this.updateLabel();
   }
 
   updateLabel() {
-    console.log(`=== updateLabel: ${this.defaultTextValue} ===`);
     this.labelTarget.textContent = this.defaultTextValue
   }
 
+  // When user selects a new file, update the label based on native file input selection
   handleNewFileSelection() {
-    // temp debug
-    console.log("=== handleNewFileSelection ===");
-    // When user selects a new file, update the label
     if (this.inputTarget.files.length > 0) {
-      console.log(`=== handleNewFileSelection: native file selection: ${this.inputTarget.files[0].name} ===`);
       this.labelTarget.textContent = this.inputTarget.files[0].name;
     } else {
-      console.log(`=== handleNewFileSelection: no file selected, delegating to updateLabel ===`);
       this.updateLabel(); // Reset label if no file selected
     }
   }
